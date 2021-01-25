4b10d161fe18ab1b67593a2e993217d5aac4697e
oaz-tech <oazit2.0@gmail.com>
2021-01-25 11:57:29 +0100

    init

---
+++ oaz-tech.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: OazTecH
+ * Description: Send newsletters to MAP subscribers.
+ * Version:     1.0.0
+ * Author:      Oussama ZERRYI ANDALOUSSI
+ * Author URI:  https://twinsgroupe.com/
+ * Php version: 7.4.9
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+//(new \Whoops\Run)->pushHandler(new \Whoops\Handler\PrettyPageHandler)->register();
