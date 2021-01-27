ecd68769776d94be1cea387d8a0b0198969eaae5
oaz-tech <oazit2.0@gmail.com>
2021-01-27 15:10:17 +0100

    fix bugs, improve performance and ux.

--- oaz-tech.php
+++ oaz-tech.php
@@ -10,4 +10,32 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-//(new \Whoops\Run)->pushHandler(new \Whoops\Handler\PrettyPageHandler)->register();
+(new \Whoops\Run)->pushHandler(new \Whoops\Handler\PrettyPageHandler)->register();
+
+add_action('admin_menu', function () {
+
+    // main menu
+    add_menu_page('OazTecH Debugger', 'Oaz TecH', 'manage_options', 'vue',
+        null, 'dashicons-text-page', 5);
+
+    // sub menus
+    add_submenu_page(
+        'vue',
+        'vue',
+        'vue',
+        'manage_options',
+        'vue',
+        fn () => plugin_dir_url(__FILE__) . '/views/admin/html_newsletter.php'
+    );
+});
+
+add_action('admin_enqueue_scripts', function () {
+    wp_enqueue_style('l1', plugin_dir_url(__FILE__) . '/dist/css/app.fb0c6e1c.css');
+    wp_enqueue_style('l2', plugin_dir_url(__FILE__) . '/dist/js/app.1549066e.js');
+    wp_enqueue_style('l3', plugin_dir_url(__FILE__) . '/dist/js/chunk-vendors.28d0d835.js');
+    wp_enqueue_style('l4', plugin_dir_url(__FILE__) . '/dist/css/app.fb0c6e1c.css');
+
+    wp_enqueue_script('l5', plugin_dir_url(__FILE__) . '/dist/js/chunk-vendors.28d0d835.js', [], '', true);
+    wp_enqueue_script('l6', plugin_dir_url(__FILE__) . '/dist/js/app.7d55ee11.js', [], '', true);
+
+});
